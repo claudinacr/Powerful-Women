8c13fc26e90f45763f8e07f91b3d99218a51c960
Claudina Cortez <claudinacr@gmail.com>
2018-01-02 20:31:02 -0300

    validacion de usuario

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,45 @@
-function user() {
+var buttonSend = document.getElementById('buttonSend');
+
+function getUser() {
   var arrayUsers = [];
   for (let i = 0; i < users.length; i++) {
     arrayUsers.push(users[i].user);
   }
   return arrayUsers;
 }
-user();
 
-function password() {
+function getPassword() {
   var arrayPassword = [];
   for (let i = 0; i < users.length; i++) {
     arrayPassword.push(users[i].password);
   }
   return arrayPassword;
 }
-password();
 
-function authentication(arrayUser, arrayPassword) {
-  var user = 'claudinacr';
-  var password = '12345';
+
+function takeInput() {
+  var user = document.getElementById("inputUser").value;
+  return user;
+}
+
+function takePassword() {
+  var password = document.getElementById("inputPassword").value;
+  return password;
+}
+
+buttonSend.onclick = function () {
+  var user = takeInput();
+  var pasword = takePassword();
+  authentication(getUser(), getPassword(), user, pasword);
+};
+
+
+
+
+
+
+
+function authentication(arrayUser, arrayPassword, user, password) {
   var validarUser, validarPassword;
   for (let i = 0; i < arrayUser.length; i++) {
     if (user === arrayUser[i]) {
@@ -33,7 +54,9 @@ function authentication(arrayUser, arrayPassword) {
     }
   }
   if (validarUser === true && validarPassword === true) {
-    console.log('usuario registrado. Puede Ingresar');
+    console.log('usuario registrado');
+    window.open('redSocial.html', '_self');
+  } else {
+    alert('Usuario y contraseña no validos');
   }
 }
-authentication(user(), password());
