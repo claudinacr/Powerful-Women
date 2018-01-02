f119f6e6d253f6d64abe19cf260430a3a48b4d60
Claudina Cortez <claudinacr@gmail.com>
2018-01-02 17:17:09 -0300

    Maquetado

---
+++ assets/js/users.js
@@ -0,0 +1,22 @@
+var users = [{
+    "first_name": "Claudina",
+    "last_name": "Cortez",
+    "password": "12345",
+    "birth_date": "1990-03-15",
+    "age": "20",
+    "gender": "F",
+    "adress": "Venecia 1605",
+    "ocupation": "database developer ",
+  },
+  {
+    "first_name": "Carolina",
+    "last_name": "Perez",
+    "nick": "carolinaperez",
+    "password": "12345",
+    "birth_date": "1990-03-15",
+    "age": "20",
+    "gender": "F",
+    "adress": "Venecia 1605",
+    "ocupation": "database developer ",
+  }
+];
